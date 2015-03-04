e83f76d779dd3f3a6fe3155da63805df6e9622ff
Samuel Chen <samuel.net@gmail.com>
2015-03-04 21:10:51 +0800

    add strutil (hash, md5, sha1)

--- __init__.py
+++ __init__.py
@@ -11,8 +11,10 @@ Created on 1/7/2015
 from logger import Logger, log
 from singleton import SingletonBase
 from rabbitmq import RabbitMQPublisher, RabbitMQConsumer
+import strutil
 
 __all__ = [Logger, log,
            SingletonBase,
            RabbitMQPublisher, RabbitMQConsumer,
+           strutil,
 ]
